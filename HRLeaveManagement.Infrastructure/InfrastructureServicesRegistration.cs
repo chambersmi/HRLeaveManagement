aad426c577c52beeb452d2655a64db9551bf5903
Mike C <mjchambers83@gmail.com>
2025-12-29 17:31:04 -0500

    Implemented Email capabilities

---
+++ HRLeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using HRLeaveManagement.Application.Contracts.Email;
+using HRLeaveManagement.Application.Models.Email;
+using HRLeaveManagement.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRLeaveManagement.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailSender, EmailSender>(); // new instance of email sender each time its used
+
+            return services;
+        }
+    }
+}
