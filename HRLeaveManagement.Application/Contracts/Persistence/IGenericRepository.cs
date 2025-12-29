0b488cf96f9a8a183f264301a8f237984f9ce4c9
Mike C <mjchambers83@gmail.com>
2025-12-28 23:29:08 -0500

    Set up AutoMapper and MediatR

--- HRLeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
+++ HRLeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -8,7 +8,7 @@ namespace HRLeaveManagement.Application.Contracts.Persistence;
 
 public interface IGenericRepository<T> where T : class
 {
-    Task<T> GetAsync();
+    Task<List<T>> GetAsync();
     Task<T> GetByIdAsync(int id);
     Task<T> CreateAsync(T entity);
     Task<T> UpdateAsync(T entity);
---
+++ HRLeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using HRLeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    // Handle any requests of that type (GetLeaveTypesQuery) and return as a list of LeaveTypeDto
+    public class GetLeaveTypesQueryHandler : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveTypesQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            this._leaveTypeRepository = leaveTypeRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+        {
+            // Query the database
+            var leaveTypes = await _leaveTypeRepository.GetAsync();
+
+            // Convert data objects to DTO objects
+            var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+            // Return list of DTO object
+            return data;
+        }
+    }
+}
