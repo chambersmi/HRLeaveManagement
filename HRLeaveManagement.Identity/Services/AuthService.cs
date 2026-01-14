fd964499a38488241447c4005f9a0a873a3f5e68
Mike C <mjchambers83@gmail.com>
2026-01-13 22:06:26 -0500

    Created Login / GenerateJWT methods

---
+++ HRLeaveManagement.Identity/Services/AuthService.cs
@@ -0,0 +1,117 @@
+﻿using HRLeaveManagement.Application.Contracts.Identity;
+using HRLeaveManagement.Application.Exceptions;
+using HRLeaveManagement.Application.Models.Identity;
+using HRLeaveManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JwtSettings _jwtSettings;
+
+        public AuthService(
+            UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, 
+            IOptions<JwtSettings> jwtSettings)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public async Task<AuthResponse> Login(AuthRequest request)
+        {
+            // See if user exists
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            
+            if(user == null)
+            {
+                throw new NotFoundException($"User with {request.Email} not found.", request.Email);
+            }
+
+            // Can this user sign in with this password? If yes, true.
+            var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+            if(result.Succeeded == false)
+            {
+                throw new BadRequestException($"Credentials for '{request.Email}' are not valid.");
+            }
+
+            // Successfully verified the user. Generate token for user.
+            JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+
+            var response = new AuthResponse
+            {
+                Id = user.Id,
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                Email = user.Email,
+                Username = user.UserName
+            };
+
+            return response;
+        }
+
+
+        public Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+        private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+        {
+            // Retrieve user claims from DB
+            var userClaims = await _userManager.GetClaimsAsync(user);
+
+            // Retrieve user role from DB
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            // Once we send the token, everything will be seen as a claim. Claims determine permissions in the app
+
+            // Give me all the strings and select them into new objects of type Claim. The role value is stored in x
+            var roleClaims = userRoles.Select(x => new Claim(ClaimTypes.Role, x)).ToList();
+
+            // Create an array of claims that will be condensed into JWT string
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName), // Sub identifies as user, could be email, etc.
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), // Generate new Guid every login
+                new Claim(JwtRegisteredClaimNames.Email, user.Email), 
+                new Claim("uid", user.Id) // Custom field
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            // Generate symmetric security key. Encode by UTF8
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+
+            // Pass in key and hash as 256
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            // Create JWT security token
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_jwtSettings.DurationInMinutes),
+                signingCredentials: signingCredentials);
+
+            return jwtSecurityToken;
+
+        }
+    }
+}
